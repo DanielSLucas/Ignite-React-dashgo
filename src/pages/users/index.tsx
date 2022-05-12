ada2751a7ac02e6c96ed670387b3ba9c552859e7
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-12 16:37:06 -0300

    Página listas de usuários

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,118 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+import { Header } from "../../components/Header";
+import { SideBar } from "../../components/SideBar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <SideBar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button 
+              as="a" 
+              size="sm" 
+              fontSize="sm" 
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              <Tr>
+                <Td px="6"><Checkbox colorScheme="pink" /></Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Lucas</Text>  
+                    <Text fontSize="sm" color="gray.300">daniellucas-pms@hotmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  04 de Abril, 2021
+                </Td>
+                <Td>
+                <Button 
+                  as="a" 
+                  size="sm" 
+                  fontSize="sm" 
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                >
+                  Editar
+                </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6"><Checkbox colorScheme="pink" /></Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Lucas</Text>  
+                    <Text fontSize="sm" color="gray.300">daniellucas-pms@hotmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  04 de Abril, 2021
+                </Td>
+                <Td>
+                <Button 
+                  as="a" 
+                  size="sm" 
+                  fontSize="sm" 
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                >
+                  Editar
+                </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6"><Checkbox colorScheme="pink" /></Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Lucas</Text>  
+                    <Text fontSize="sm" color="gray.300">daniellucas-pms@hotmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  04 de Abril, 2021
+                </Td>
+                <Td>
+                <Button 
+                  as="a" 
+                  size="sm" 
+                  fontSize="sm" 
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                >
+                  Editar
+                </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
