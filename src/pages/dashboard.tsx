93ffbd485207f676ad555f7786273165777e0e7a
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-13 10:39:13 -0300

    responsividade do dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -71,7 +71,7 @@ export default function Dashboard() {
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
@@ -81,7 +81,7 @@ export default function Dashboard() {
           </Box>
 
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
