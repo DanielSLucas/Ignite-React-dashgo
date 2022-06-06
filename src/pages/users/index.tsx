4bff4070afab924e0b891b1285db63c075a32bb4
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-06-06 17:12:55 -0300

    hook de listagem

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,34 +17,15 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from "react-query";
+
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/userUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, isError: error } = useQuery('users', async () => {
-    const { data } = await api.get('users');    
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 sec
-  })    
+  const { data, isLoading, isFetching, isError: error } = useUsers()  
 
   const isWideVersion = useBreakpointValue({
     base: false,
