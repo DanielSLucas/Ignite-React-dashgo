b49f970ed8cc4a5eeda9c9a292cd603dba4b3c8f
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-06-06 18:54:49 -0300

    SSR no React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
   Tr, 
   useBreakpointValue 
 } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
 import NextLink from "next/link";
 import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
@@ -25,12 +26,27 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { api } from "../../services/api";
-import { useUsers } from "../../services/hooks/userUsers";
+import { getUsers, useUsers } from "../../services/hooks/userUsers";
 import { queryClient } from "../../services/queryClient";
 
-export default function UserList() {
+type UsersListProps = {
+  users: {
+    id: string;
+    name: string;
+    email: string;
+    created_at: string;
+  }[];
+  totalCount: number;
+}
+
+export default function UserList({ users, totalCount }: UsersListProps) {
   const [page, setPage] = useState(1);
-  const { data, isLoading, isFetching, isError: error } = useUsers(page)  
+  const { data, isLoading, isFetching, isError: error } = useUsers(page, {
+    initialData: {
+      users,
+      totalCount,
+    },    
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -141,4 +157,15 @@ export default function UserList() {
       </Flex>
     </Box>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1)
+  
+  return {
+    props: {
+      users,
+      totalCount,
+    }
+  }
 }
--- src/services/hooks/userUsers.ts
+++ src/services/hooks/userUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query";
 import { api } from "../api";
 
 type User = {
@@ -41,8 +41,9 @@ export async function getUsers (page: number): Promise<GetUsersResponse> {
   };
 }
 
-export function useUsers(page: number) {
+export function useUsers(page: number, options: UseQueryOptions) {
   return useQuery(['users', page], () => getUsers(page), {
     staleTime: 1000 * 60 * 10, // 10 min
-  }) 
+    ...options,
+  }) as UseQueryResult<GetUsersResponse, unknown>;
 }
