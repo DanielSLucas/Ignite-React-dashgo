11bc009a8bebfe7a213e8614c10142c95b459d96
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-06-06 16:52:12 -0300

    configurando DevTools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SideabrDrawerContext';
@@ -20,6 +21,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+      
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 };
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -42,6 +42,8 @@ export default function UserList() {
     });
 
     return users;
+  }, {
+    staleTime: 1000 * 5, // 5 sec
   })    
 
   const isWideVersion = useBreakpointValue({
