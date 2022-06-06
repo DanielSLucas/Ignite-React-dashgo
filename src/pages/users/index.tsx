d52befbb49f040579e5418a90dff542e69c5ba9f
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-06-06 16:47:43 -0300

    listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -28,8 +28,21 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
-    return data;
-  })  
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    });
+
+    return users;
+  })    
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -83,75 +96,31 @@ export default function UserList() {
               </Thead>
 
               <Tbody>
-                <Tr>
-                  <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Daniel Lucas</Text>  
-                      <Text fontSize="sm" color="gray.300">daniellucas-pms@hotmail.com</Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                  {isWideVersion && (
+                {data.map(user => (
+                  <Tr key={user.id}>
+                    <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
                     <Td>
-                      <Button 
-                        as="a" 
-                        size="sm" 
-                        fontSize="sm" 
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      >
-                        Editar
-                      </Button>
+                      <Box>
+                        <Text fontWeight="bold">{user.name}</Text>  
+                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                      </Box>
                     </Td>
-                  )}
-                </Tr>
-                <Tr>
-                  <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Daniel Lucas</Text>  
-                      <Text fontSize="sm" color="gray.300">daniellucas-pms@hotmail.com</Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                  {isWideVersion && (
-                    <Td>
-                      <Button 
-                        as="a" 
-                        size="sm" 
-                        fontSize="sm" 
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      >
-                        Editar
-                      </Button>
-                    </Td>
-                  )}
-                </Tr>
-                <Tr>
-                  <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Daniel Lucas</Text>  
-                      <Text fontSize="sm" color="gray.300">daniellucas-pms@hotmail.com</Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                  {isWideVersion && (
-                    <Td>
-                      <Button 
-                        as="a" 
-                        size="sm" 
-                        fontSize="sm" 
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      >
-                        Editar
-                      </Button>
-                    </Td>
-                  )}
-                </Tr>
+                    {isWideVersion && <Td>{user.createdAt}</Td>}
+                    {isWideVersion && (
+                      <Td>
+                        <Button 
+                          as="a" 
+                          size="sm" 
+                          fontSize="sm" 
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    )}
+                  </Tr> 
+                ))}
               </Tbody>
             </Table>
 
